3c715ec7bf5e6d47f0b32a7f69b3b3bc120103d5
Crane-tsuru <116482604+Crane-tsuru@users.noreply.github.com>
2024-09-12 16:50:14 +0900

    update Swift Project

---
+++ Project/Project/ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectApp.swift
+//  Project
+//
+//  Created by 鶴見駿 on 2024/09/12.
+//
+
+import SwiftUI
+
+@main
+struct ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
